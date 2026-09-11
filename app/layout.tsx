6a3cdfd0b4e208063dac173bd00a6982ef38041c
Brian chelule <116880584+Brian-17@users.noreply.github.com>
2026-09-11 18:58:58 +0300

    Add RootLayout component with ClerkProvider

---
+++ app/layout.tsx
@@ -0,0 +1,5 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import "./globals.css";
+export default function RootLayout({children}:{children:React.ReactNode}){
+  return (<ClerkProvider><html lang="en"><body className="bg-[#050507]">{children}</body></html></ClerkProvider>);
+}
