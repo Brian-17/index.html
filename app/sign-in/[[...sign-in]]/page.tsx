b79b27028e14d25bd945674d014ce208f4b06cfd
Brian chelule <116880584+Brian-17@users.noreply.github.com>
2026-09-12 16:28:35 +0300

    Refactor SignIn page layout and styling

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,4 +1,2 @@
 import { SignIn } from "@clerk/nextjs";
-export default function Page() {
-  return <div style={{display:"flex",justifyContent:"center",paddingTop:"100px"}}><SignIn /></div>
-}
+export default function Page(){ return <div className="min-h-screen bg-[#050507] flex items-center justify-center"><SignIn /></div> }
