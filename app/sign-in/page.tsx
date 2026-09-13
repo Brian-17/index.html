a79c078895614a19ab7bf120c040dd374acdfc4d
Brian chelule <116880584+Brian-17@users.noreply.github.com>
2026-09-13 12:58:42 +0300

    Delete app/sign-in directory

--- app/sign-in/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignIn } from "@clerk/nextjs";
-
-export default function Page() {
-  return (
-    <div style={{display:"flex", justifyContent:"center", alignItems:"center", minHeight:"100vh", background:"#050507"}}>
-      <SignIn />
-    </div>
-  );
-}
